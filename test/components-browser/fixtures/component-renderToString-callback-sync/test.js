2acf8141588459378b31b5fde3610eaf199afaa7
kale-io <cos+a@iuvo.ai>
2019-03-07 23:11:28 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/components-browser/fixtures/component-renderToString-callback-sync/test.js
+++ test/components-browser/fixtures/component-renderToString-callback-sync/test.js
@@ -1,16 +1,16 @@
 var expect = require("chai").expect;
 
-module.exports = function(helpers, done) {
+module.exports = (helpers, done) => {
     var component = helpers.mount(require.resolve("./index"), {
         name: "john"
     });
 
-    component.on("html", function(renderedHtml) {
+    component.on("html", renderedHtml => {
         expect(renderedHtml).to.equal("<div>[async] john</div>");
         done();
     });
 
-    component.on("error", function(error) {
+    component.on("error", error => {
         done(error);
     });
 };
