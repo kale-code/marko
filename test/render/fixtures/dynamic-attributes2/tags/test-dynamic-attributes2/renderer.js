16649388fd35a2d1cf432c16d4b44d91749f4e78
kale-io <cos+a@iuvo.ai>
2019-03-07 21:57:09 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/render/fixtures/dynamic-attributes2/tags/test-dynamic-attributes2/renderer.js
+++ test/render/fixtures/dynamic-attributes2/tags/test-dynamic-attributes2/renderer.js
@@ -1,12 +1,10 @@
-exports.render = function(input, out) {
+exports.render = (input, out) => {
     out.write("test: " + input.test + "|");
     var dynamicAttributes = input["*"];
 
     if (dynamicAttributes) {
         var keys = Object.keys(dynamicAttributes).sort();
-        var entries = keys.map(function(key) {
-            return key + "=" + dynamicAttributes[key];
-        });
+        var entries = keys.map(key => key + "=" + dynamicAttributes[key]);
         out.write("dynamic attributes: [" + entries.join(", ") + "]");
     } else {
         out.write("dynamic attributes: []");
