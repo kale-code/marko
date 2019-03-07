ce138710260e1c37cdb8bec96b3ac262db758621
kale-io <cos+a@iuvo.ai>
2019-03-07 23:11:36 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/components-browser/fixtures/custom-events-declaritive-once/test.js
+++ test/components-browser/fixtures/custom-events-declaritive-once/test.js
@@ -1,11 +1,11 @@
 var expect = require("chai").expect;
 
-module.exports = function(helpers) {
+module.exports = helpers => {
     var component = helpers.mount(require.resolve("./index"), {});
 
     var numTestEventFired = 0;
 
-    component.handleTestEvent1 = function(a, b) {
+    component.handleTestEvent1 = (a, b) => {
         expect(a).to.equal("a");
         expect(b).to.equal("b");
         numTestEventFired++;
