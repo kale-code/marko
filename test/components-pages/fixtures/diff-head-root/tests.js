0e31c7ad751ab23ba98ed9cbaef18987246e5fea
kale-io <cos+a@iuvo.ai>
2019-03-07 21:32:13 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/components-pages/fixtures/diff-head-root/tests.js
+++ test/components-pages/fixtures/diff-head-root/tests.js
@@ -1,8 +1,8 @@
 var path = require("path");
 var expect = require("chai").expect;
 
-describe(path.basename(__dirname), function() {
-    it("should allow diffing head at the root", function() {
+describe(path.basename(__dirname), () => {
+    it("should allow diffing head at the root", () => {
         var app = window.app;
         expect(document.head.getAttribute("count")).to.equal("0");
 
