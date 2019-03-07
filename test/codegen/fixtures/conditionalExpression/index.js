aec703f5c024a0b45de1f8fcb36859c9c47e7244
kale-io <cos+a@iuvo.ai>
2019-03-07 21:27:20 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/codegen/fixtures/conditionalExpression/index.js
+++ test/codegen/fixtures/conditionalExpression/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
-module.exports = function(builder) {
-    return builder.conditionalExpression(
+module.exports = builder => builder.conditionalExpression(
         builder.identifier("isHidden"),
         builder.literal("hidden"),
         builder.literal("visible")
     );
-};
