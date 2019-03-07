fee45eebb7e815fa9dbd7fea0a02e83862f499e2
kale-io <cos+a@iuvo.ai>
2019-03-07 21:56:32 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/markoc/fixtures/single-xml-template/test.js
+++ test/markoc/fixtures/single-xml-template/test.js
@@ -1,6 +1,6 @@
 var expect = require("chai").expect;
 
-exports.test = function(helpers) {
+exports.test = helpers => {
     expect(helpers.existsSync("template.marko.xml.js")).to.equal(false);
     var result = helpers.spawnSync(["template.marko.xml"]);
     expect(helpers.existsSync("template.marko.xml.js")).to.equal(true);
