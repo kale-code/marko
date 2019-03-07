aadc76be5b6d0b4584174c036b907c48fd935eeb
kale-io <cos+a@iuvo.ai>
2019-03-07 23:09:26 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/api/fixtures/render-await-client-reorder-unhandled-rejected-promise/test.js
+++ test/api/fixtures/render-await-client-reorder-unhandled-rejected-promise/test.js
@@ -1,13 +1,13 @@
 var nodePath = require("path");
 
-exports.check = function(marko, markoCompiler, expect, snapshot, done) {
+exports.check = (marko, markoCompiler, expect, snapshot, done) => {
     var template = marko.load(nodePath.join(__dirname, "template.marko"));
     var out = template.createOut();
 
     template.render(
         {
             userPromise: new Promise((_, reject) => {
-                setTimeout(function() {
+                setTimeout(() => {
                     reject(new Error("User Promise Rejected Error"));
                 }, 10);
             })
