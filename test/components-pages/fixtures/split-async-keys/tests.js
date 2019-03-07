0086a31e4b846b7da34c2cb86770d6398dd37f3e
kale-io <cos+a@iuvo.ai>
2019-03-07 21:32:32 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/components-pages/fixtures/split-async-keys/tests.js
+++ test/components-pages/fixtures/split-async-keys/tests.js
@@ -1,8 +1,8 @@
 var path = require("path");
 var expect = require("chai").expect;
 
-describe(path.basename(__dirname), function() {
-    it("should initialize components correctly across async boundaries", function(done) {
+describe(path.basename(__dirname), () => {
+    it("should initialize components correctly across async boundaries", done => {
         expect(window.component.getEl("div")).to.not.equal(undefined);
         expect(window.component.getComponent("child")).to.not.equal(undefined);
         done();
