1280a03e9570a60bf46e555e7bdc0f7e80f71785
kale-io <cos+a@iuvo.ai>
2019-03-07 21:55:09 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/components-pages/fixtures/await-surround-html/tests.js
+++ test/components-pages/fixtures/await-surround-html/tests.js
@@ -1,8 +1,8 @@
 var path = require("path");
 var expect = require("chai").expect;
 
-describe(path.basename(__dirname), function() {
-    it("should mount components", function() {
+describe(path.basename(__dirname), () => {
+    it("should mount components", () => {
         expect(window.helloComponent != null).to.equal(true);
     });
 });
