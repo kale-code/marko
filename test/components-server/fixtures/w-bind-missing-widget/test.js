0ba31b6c43bc8b6f4f9a30b99d7c65ef3bcba158
kale-io <cos+a@iuvo.ai>
2019-03-07 21:32:59 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- test/components-server/fixtures/w-bind-missing-widget/test.js
+++ test/components-server/fixtures/w-bind-missing-widget/test.js
@@ -1,7 +1,7 @@
 var expect = require("chai").expect;
 
-module.exports = function() {
-    expect(function() {
+module.exports = () => {
+    expect(() => {
         require("./template.marko");
     }).to.throw(
         /No corresponding JavaScript module found in the same directory/
